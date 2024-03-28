de45afb28227840f7f7a68b1b489d0c1bab6b0a0
bzdhxs <bzdhxsnt01@gmail.com>
2024-03-29 00:42:18 +0800

    解析异常栈信息

--- start/source/kernel/include/cpu/irq.h
+++ start/source/kernel/include/cpu/irq.h
@@ -1,9 +1,23 @@
 #ifndef IRQ_H
 #define IRQ_H
+#include "comm/types.h"
 
 
+/**
+ * 中断发生时相应的栈结构，暂时为无特权级发生的情况
+ */
+typedef struct _exception_frame_t{
+    // 结合压栈的过程，以及pusha指令的实际压入过程
+    uint32_t gs,fs,es,ds;
+    
+    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+
+    uint32_t eip,cs,eflags;
+
+}exception_frame_t;
 
 void irq_init(void);
+
 
 
 #endif //IRQ_H
