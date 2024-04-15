f3273be7e2bfa6a725715ba4477ebc967c41a01d
bzdhxs <bzdhxsnt01@gmail.com>
2024-04-15 17:15:13 +0800

    开启内存分页机制

--- start/source/comm/cpu_instr.h
+++ start/source/comm/cpu_instr.h
@@ -2,104 +2,139 @@
 #define CPU_INSTR_H
 #include "types.h"
 
-static inline uint8_t inb(uint16_t port) {
+static inline uint8_t inb(uint16_t port)
+{
     // inb al,dx  从I/O端口dx 读取一个字节
-    uint8_t rv; 
-    __asm__ __volatile__("inb %[p],%[v]":[v]"=a"(rv):[p]"d"(port));
+    uint8_t rv;
+    __asm__ __volatile__("inb %[p],%[v]" : [v] "=a"(rv) : [p] "d"(port));
     return rv;
 }
 
-static inline void outb(uint16_t port,uint8_t data){
+static inline void outb(uint16_t port, uint8_t data)
+{
     // outb al,dx  向I/O端口al ,写入一个字节(dx) data
-    __asm__ __volatile__("outb %[v],%[p]"::[p]"d"(port),[v]"a"(data));
+    __asm__ __volatile__("outb %[v],%[p]" ::[p] "d"(port), [v] "a"(data));
 }
-static inline uint16_t inw(uint16_t port){
+static inline uint16_t inw(uint16_t port)
+{
     // in ax dx
-    uint16_t rv; 
-    __asm__ __volatile__("in %[p],%[v]":[v]"=a"(rv):[p]"d"(port));
+    uint16_t rv;
+    __asm__ __volatile__("in %[p],%[v]" : [v] "=a"(rv) : [p] "d"(port));
     return rv;
 }
 
-static inline void cli(void){
-    __asm__ __volatile__("cli");    
+static inline void cli(void)
+{
+    __asm__ __volatile__("cli");
 }
 
-static inline void sti(void){
-    __asm__ __volatile__("sti");    
+static inline void sti(void)
+{
+    __asm__ __volatile__("sti");
 }
 
+static inline void lgdt(uint32_t start, uint32_t size)
+{
 
-static inline void lgdt(uint32_t start,uint32_t size){
-
-    struct {
+    struct
+    {
         uint16_t limit;
         uint16_t start15_0;
         uint16_t start32_16;
-    }gdt;
-    
+    } gdt;
+
     gdt.start32_16 = start >> 16;
     gdt.start15_0 = start & 0xffff;
     gdt.limit = size - 1;
 
-    __asm__ __volatile__("lgdt %[g]"::[g]"m"(gdt));
+    __asm__ __volatile__("lgdt %[g]" ::[g] "m"(gdt));
 }
 
-
-static inline uint32_t read_cr0(void){
+static inline uint32_t read_cr0(void)
+{
     uint32_t cr0;
-    __asm__ __volatile__("mov %%cr0, %[v]":[v]"=r"(cr0));
+    __asm__ __volatile__("mov %%cr0, %[v]" : [v] "=r"(cr0));
     return cr0;
 }
 
+static inline void write_cr0(uint32_t v)
+{
 
-static inline void write_cr0(uint32_t v){
-
-    __asm__ __volatile__("mov %[v],%%cr0"::[v]"r"(v));
+    __asm__ __volatile__("mov %[v],%%cr0" ::[v] "r"(v));
 }
 
-
-static inline void far_jump(uint32_t selector,uint32_t offset){
-    uint32_t addr[] = {offset,selector};
-    __asm__ __volatile__("ljmpl *(%[a])"::[a]"r"(addr));
+static inline uint32_t read_cr3(void)
+{
+    uint32_t cr3;
+    __asm__ __volatile__("mov %%cr3, %[v]" : [v] "=r"(cr3));
+    return cr3;
 }
 
+static inline void write_cr3(uint32_t v)
+{
 
+    __asm__ __volatile__("mov %[v],%%cr3" ::[v] "r"(v));
+}
+static inline uint32_t read_cr4(void)
+{
+    uint32_t cr4;
+    __asm__ __volatile__("mov %%cr4, %[v]" : [v] "=r"(cr4));
+    return cr4;
+}
 
-static inline void lidt(uint32_t start,uint32_t size){
+static inline void write_cr4(uint32_t v)
+{
 
-    struct {
+    __asm__ __volatile__("mov %[v],%%cr4" ::[v] "r"(v));
+}
+
+static inline void far_jump(uint32_t selector, uint32_t offset)
+{
+    uint32_t addr[] = {offset, selector};
+    __asm__ __volatile__("ljmpl *(%[a])" ::[a] "r"(addr));
+}
+
+static inline void lidt(uint32_t start, uint32_t size)
+{
+
+    struct
+    {
         uint16_t limit;
         uint16_t start15_0;
         uint16_t start32_16;
-    }idt;
-    
+    } idt;
+
     idt.start32_16 = start >> 16;
     idt.start15_0 = start & 0xffff;
     idt.limit = size - 1;
 
-    __asm__ __volatile__("lidt %[g]"::[g]"m"(idt));
+    __asm__ __volatile__("lidt %[g]" ::[g] "m"(idt));
 }
 
 /**
  * @brief 当处理器执行到HLT指令时，它会停止执行并进入一个低功耗状态，直到外部事件唤醒处理器继续执行。
- * 
+ *
  */
-static inline void hlt (void) {
+static inline void hlt(void)
+{
     __asm__ __volatile__("hlt");
 }
 
-static inline void write_tr(uint16_t tss_sel) {
-    __asm__ __volatile__ ("ltr %%ax"::"a"(tss_sel));
+static inline void write_tr(uint16_t tss_sel)
+{
+    __asm__ __volatile__("ltr %%ax" ::"a"(tss_sel));
 }
 
-static inline uint32_t read_eflags (void) {
+static inline uint32_t read_eflags(void)
+{
     uint32_t eflags;
-    __asm__ __volatile__ ("pushf\n\tpop %%eax":"=a"(eflags));    
+    __asm__ __volatile__("pushf\n\tpop %%eax" : "=a"(eflags));
     return eflags;
 }
 
-static inline void write_eflags (uint32_t eflags) {
-    __asm__ __volatile__ ("push %%eax\n\tpopf"::"a"(eflags));  
+static inline void write_eflags(uint32_t eflags)
+{
+    __asm__ __volatile__("push %%eax\n\tpopf" ::"a"(eflags));
 }
 
 #endif // CPU_INSTR_H
--- start/source/loader/loader_32.c
+++ start/source/loader/loader_32.c
@@ -1,49 +1,50 @@
- /*
+/*
  * 自己动手写操作系统
  *
  * 32位引导代码
- * 二级引导，负责进行硬件检测，进入保护模式，然后加载内核，并跳转至内核运行 
+ * 二级引导，负责进行硬件检测，进入保护模式，然后加载内核，并跳转至内核运行
  */
-
-
 
 #include "loader.h"
 #include "comm/elf.h"
 /**
-    使用LBA48位模式读取磁盘
+	使用LBA48位模式读取磁盘
 	将内核数据存入 从100号磁盘开始 读入到内存0x100000
-    参数设置参考手册
+	参数设置参考手册
 */
-static void read_disk(int sector,int sector_count,uint8_t *buf){
-    outb(0x1F6, (uint8_t) (0xE0));
+static void read_disk(int sector, int sector_count, uint8_t *buf)
+{
+	outb(0x1F6, (uint8_t)(0xE0));
 
-	outb(0x1F2, (uint8_t) (sector_count >> 8));
-    outb(0x1F3, (uint8_t) (sector >> 24));		// LBA参数的24~31位
-    outb(0x1F4, (uint8_t) (0));					// LBA参数的32~39位
-    outb(0x1F5, (uint8_t) (0));					// LBA参数的40~47位
+	outb(0x1F2, (uint8_t)(sector_count >> 8));
+	outb(0x1F3, (uint8_t)(sector >> 24)); // LBA参数的24~31位
+	outb(0x1F4, (uint8_t)(0));			  // LBA参数的32~39位
+	outb(0x1F5, (uint8_t)(0));			  // LBA参数的40~47位
 
-    outb(0x1F2, (uint8_t) (sector_count));
-	outb(0x1F3, (uint8_t) (sector));			// LBA参数的0~7位
-	outb(0x1F4, (uint8_t) (sector >> 8));		// LBA参数的8~15位
-	outb(0x1F5, (uint8_t) (sector >> 16));		// LBA参数的16~23位
+	outb(0x1F2, (uint8_t)(sector_count));
+	outb(0x1F3, (uint8_t)(sector));		  // LBA参数的0~7位
+	outb(0x1F4, (uint8_t)(sector >> 8));  // LBA参数的8~15位
+	outb(0x1F5, (uint8_t)(sector >> 16)); // LBA参数的16~23位
 
-	outb(0x1F7, (uint8_t) 0x24);  // 发送命令
-
+	outb(0x1F7, (uint8_t)0x24); // 发送命令
 
 	// 读取数据
-	uint16_t *data_buf = (uint16_t*) buf;
-	while (sector_count-- > 0) {
+	uint16_t *data_buf = (uint16_t *)buf;
+	while (sector_count-- > 0)
+	{
 		// 每次扇区读之前都要检查，等待数据就绪 根据手册中的Status register 来判断 取出 DRQ
-		while ((inb(0x1F7) & 0x88) != 0x8) {}
+		while ((inb(0x1F7) & 0x88) != 0x8)
+		{
+		}
 
 		// 读取并将数据写入到缓存中
-		for (int i = 0; i < SECTOR_SIZE / 2; i++) {
-            // inw指令  0X1F0:位数据寄存器
+		for (int i = 0; i < SECTOR_SIZE / 2; i++)
+		{
+			// inw指令  0X1F0:位数据寄存器
 			*data_buf++ = inw(0x1F0);
 		}
 	}
 }
-
 
 /**
  * 解析elf文件，提取内容到相应的内存中
@@ -52,65 +53,93 @@ static void read_disk(int sector,int sector_count,uint8_t *buf){
  * @return
  */
 
-// file_buffer = SYS_KERNEL_LOAD_ADDR = 0x100000 
+// file_buffer = SYS_KERNEL_LOAD_ADDR = 0x100000
 
-static uint32_t reload_elf_file (uint8_t * file_buffer){
+static uint32_t reload_elf_file(uint8_t *file_buffer)
+{
 	// 读取的只是ELF文件，不像BIN那样可直接运行，需要从中加载出有效数据和代码
-    // 简单判断是否是合法的ELF文件
+	// 简单判断是否是合法的ELF文件
 
-	Elf32_Ehdr * elf_hdr = (Elf32_Ehdr *)file_buffer;
-	
-	if((elf_hdr->e_ident[0] != 0x7f) || (elf_hdr->e_ident[1] != 'E' || (elf_hdr->e_ident[2] != 'L') 
-	|| (elf_hdr->e_ident[3] != 'F'))){
+	Elf32_Ehdr *elf_hdr = (Elf32_Ehdr *)file_buffer;
+
+	if ((elf_hdr->e_ident[0] != 0x7f) || (elf_hdr->e_ident[1] != 'E' || (elf_hdr->e_ident[2] != 'L') || (elf_hdr->e_ident[3] != 'F')))
+	{
 		return 0;
 	}
 
 	// 然后从中加载程序头，将内容拷贝到相应的位置
-	for(int i = 0 ;i < elf_hdr->e_phnum;i++){
-		Elf32_Phdr * phdr = (Elf32_Phdr *)(file_buffer +elf_hdr->e_phoff) + i;
-		
+	for (int i = 0; i < elf_hdr->e_phnum; i++)
+	{
+		Elf32_Phdr *phdr = (Elf32_Phdr *)(file_buffer + elf_hdr->e_phoff) + i;
+
 		// 不是可加载的类型 就不往内存里拷贝
-		if(phdr->p_type != PT_LOAD){
+		if (phdr->p_type != PT_LOAD)
+		{
 			continue;
 		}
 
-	// ELF文件加载到内存 
+		// ELF文件加载到内存
 		uint8_t *src = file_buffer + phdr->p_offset;
-		uint8_t *dest = (uint8_t *) phdr->p_paddr;
-		for(int j = 0;j<phdr->p_filesz;j++ ){
+		uint8_t *dest = (uint8_t *)phdr->p_paddr;
+		for (int j = 0; j < phdr->p_filesz; j++)
+		{
 			*dest++ = *src++;
 		}
 
 		dest = (uint8_t *)phdr->p_paddr + phdr->p_filesz;
-		for(int j  = 0;j < phdr->p_memsz - phdr->p_filesz;j++){
+		for (int j = 0; j < phdr->p_memsz - phdr->p_filesz; j++)
+		{
 			*dest++ = 0;
 		}
 
-		return elf_hdr ->e_entry;
-
-
+		return elf_hdr->e_entry;
 	}
 }
 
-
-static void dile(int code){
-	for(;;){}
+static void dile(int code)
+{
+	for (;;)
+	{
+	}
 }
 
+#define PDE_P (1 << 0)
+#define PDE_PS (1 << 7)
+#define PDE_W (1 << 1)
+#define CR4_PSE (1 << 4)
+#define CR0_PG (1 << 31)
 
+void enable_page_mode(void)
+{
+	static uint32_t page_dir[1024] __attribute__((aligned(4096))) = {
+		[0] = PDE_P | PDE_PS | PDE_W | 0
 
-void load_kernel(void){
+	};
+
+	uint32_t cr4 = read_cr4();
+	write_cr4(cr4 | CR4_PSE);
+	write_cr3((uint32_t)page_dir);
+	write_cr0(read_cr0() | CR0_PG); // 开启分页机制 CR0_PG
+}
+void load_kernel(void)
+{
 	// 读取的扇区数一定要大一些,保不准kernel.elf大小会变得很大
 	// 从100号扇区开始 读500个扇区
-    read_disk(100,500,(uint8_t *)SYS_KERNEL_LOAD_ADDR);
-     // 解析ELF文件，并通过调用的方式，进入到内核中去执行，同时传递boot参数
-	 // 临时将elf文件先读到SYS_KERNEL_LOAD_ADDR处，再进行解析
+	read_disk(100, 500, (uint8_t *)SYS_KERNEL_LOAD_ADDR);
+	// 解析ELF文件，并通过调用的方式，进入到内核中去执行，同时传递boot参数
+	// 临时将elf文件先读到SYS_KERNEL_LOAD_ADDR处，再进行解析
 	uint32_t kernel_entry = reload_elf_file((uint8_t *)SYS_KERNEL_LOAD_ADDR);
 	// 死机
-	if(!kernel_entry){
+	if (!kernel_entry)
+	{
 		dile(-1);
 	}
+
+	enable_page_mode();
+
 	// 转换为函数指针，然后跳进内核
 	((void (*)(boot_info_t *))kernel_entry)(&boot_info);
-    for(;;){}
+	for (;;)
+	{
+	}
 }
